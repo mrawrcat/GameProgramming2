bbb2e8a4ca99780fccd6a01d3527732d0544a795
mrawrcat <wdan13@gmail.com>
2019-03-14 16:07:47 -0400

    added new tutorial pics

---
+++ Project 0 Platformer/Assets/Scripts/GameHelper.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameHelper : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //GameManager.Gmgr.Countdown();
+    }
+    public void callGmgrSceneSwitch(string scene)
+    {
+        GameManager.Gmgr.scneSwitch(scene);
+    }
+}
