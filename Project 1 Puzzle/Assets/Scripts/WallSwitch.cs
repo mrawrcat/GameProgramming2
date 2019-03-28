ae6ffc35af02bce9276fb1058e09048680ac8679
mrawrcat <wdan13@gmail.com>
2019-03-28 17:46:29 -0400

    added some buttons and walls

---
+++ Project 1 Puzzle/Assets/Scripts/WallSwitch.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallSwitch : MonoBehaviour
+{
+    private MoveWall moveplatformscript;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        moveplatformscript = FindObjectOfType<MoveWall>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Ghost")
+        {
+            Debug.Log("stepped on switch");
+            moveplatformscript.open = true;
+        }
+    }
+
+}
